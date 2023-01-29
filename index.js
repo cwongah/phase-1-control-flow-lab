29136ce84941f5c1c4e7e7cea79630ba889b3a41
Christopher Wong <cw30355@gmail.com>
2023-01-29 13:31:33 -0500

    Completed lab

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let response
+  if (feet <= 400){
+    response = "This one is on me!"
+  }
+  else if (feet > 400 && feet < 2000){
+    response = "That will be twenty bucks."
+  }
+  else if (feet > 2000 && feet <= 2500){
+    response = "I will gladly take your thirty bucks."
+  }
+  else if (feet > 2500){
+    response = "No can do."
+  }
+  return response
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let response = city === "NYC" ? "Ok, sounds good.": "No go."
+  return response
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response
+  switch (tip) {
+    case "generous":
+      response = "Thank you so much."
+      break;
+    case "not as generous":
+      response = "Thank you."
+      break;  
+    default:
+      response = "Bye."
+      break;
+  }
+  return response
 }
